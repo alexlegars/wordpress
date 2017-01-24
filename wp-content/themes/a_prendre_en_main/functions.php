0ab989134e0ba3aab9de5c3124c3218fa325e56d
alexlegars <alexlegars@gmail.com>
2017-01-24 14:48:50 +0100

    add scss watcher

--- wp-content/themes/a_prendre_en_main/functions.php
+++ wp-content/themes/a_prendre_en_main/functions.php
@@ -26,3 +26,49 @@ function sk_after_setup_theme()
 }
 
 
+
+
+define('WP_SCSS_ALWAYS_RECOMPILE', true);
+
+
+add_action('wp_enqueue_scripts', 'al_scripts');
+
+function al_scripts()
+{
+    $version = 2;
+
+    /* CSS*/
+
+    wp_register_style('css', get_stylesheet_directory_uri() . 'assets/css/main.css', array(), $version, 'all');
+    wp_enqueue_style('css');
+
+//    wp_register_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array(), $version, 'all');
+//    wp_enqueue_style('bootstrap');
+//
+//    wp_register_style('font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css', array(), $version, 'all');
+//    wp_enqueue_style('font-awesome');
+//
+//    wp_register_style('lightbox', get_stylesheet_directory_uri() . '/css/lightbox.css', array(), $version, 'all');
+//    wp_enqueue_style('lightbox');
+//
+//    wp_register_style('main', get_stylesheet_directory_uri() . '/css/main.css', array(), $version, 'all');
+//    wp_enqueue_style('main');
+//
+//    wp_register_style('prettyPhoto', get_stylesheet_directory_uri() . '/css/prettyPhoto.css', array(), $version, 'all');
+//    wp_enqueue_style('prettyPhoto');
+//
+//    wp_register_style('responsive', get_stylesheet_directory_uri() . '/css/responsive.css', array(), $version, 'all');
+//    wp_enqueue_style('responsive');
+
+
+    /* JS */
+
+//    wp_register_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.js', array(), $version, 'all');
+//    wp_enqueue_script('jquery');
+
+
+
+
+
+}
+
