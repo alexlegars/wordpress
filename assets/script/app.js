f9b7febf420607521b6dfabd0f9d24e81d89b3b5
Trapdafifou <t.marsal@mutum.fr>
2017-01-26 01:06:28 +0100

    [ADD] Scene1 and 2 + responsive

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,42 +3,74 @@
  */
 
 boySprite = [
-    './assets/img/boy-1.svg',
-    './assets/img/boy-2.svg',
-    './assets/img/boy-3.svg',
-    './assets/img/boy-2.svg'
+    './assets/img/scene1/boy-1.svg',
+    './assets/img/scene1/boy-2.svg',
+    './assets/img/scene1/boy-3.svg',
+    './assets/img/scene1/boy-2.svg'
 ];
 
-
-function sprite (imageTab){
-   var index;
-    index >= imageTab.length ? index+=1 : index = 0;
-    imageTab[index];
-}
-
-var element1 = $('.act_first');
-var parkour = 0;
+workerSprite = [
+    './assets/img/scene1/worker-1.svg',
+    './assets/img/scene1/worker-2.svg',
+    './assets/img/scene1/worker-3.svg',
+    './assets/img/scene1/worker-2.svg'
+];
 
 var controller = new ScrollMagic.Controller();
-var element = $('.act_second');
+
+//SCENE 1 TWEENS
+var element = $('.act_first');
+var boySwap = new TimelineMax({repeat: 11});
+boySwap
+    .set(element.find('.img--boy'), {attr: {src: boySprite[0]}}, 0)
+    .set(element.find('.img--boy'), {attr: {src: boySprite[1]}}, 100)
+    .set(element.find('.img--boy'), {attr: {src: boySprite[2]}}, 200)
+    .set(element.find('.img--boy'), {attr: {src: boySprite[3]}}, 300);
+
+var scene1 = new TimelineMax();
+scene1.to(element.find('.img--boy'), 1000, {left: '46%'}, 2000)
+    .to(element.find('.img--boy'), 500, {bottom: 50, width:'-=2%'}, 2500)
+    .to(element.find('.img--puit'), 500, {left:'40%'},1500)
+    .to(element.find('.img--puit'), 1000, {opacity: 1}, 4000);
 
 
-var scene2 = new TimelineMax({repeat:3});
-scene2
-    .set(element.find('img'), {attr:{src: boySprite[0]}}, 1)
-    .set(element.find('img'), {attr:{src: boySprite[1]}}, 2)
-    .set(element.find('img'), {attr:{src: boySprite[2]}}, 3)
-    .set(element.find('img'), {attr:{src: boySprite[3]}}, 4)
-    .to(element, 1, {left : '+=10%'}, -1);
-var scene2Controller = new ScrollMagic.Scene({
-    duration: 2400,
-    offset: 150,
+var scene1Bg = new TimelineMax({repeat: 0});
+scene1Bg
+    .to(element.find('.img--bg'), 4000, {right: '0%', ease:Linear.easeNone})
+    .add(function () {console.log(element.find('.img--puit'))});
+
+
+
+//SCENE 2 TWEENS
+
+var workerSwap = new TimelineMax({repeat: 22});
+workerSwap
+    .set(element.find('.img--worker'), {attr: {src: workerSprite[0]}}, 0)
+    .set(element.find('.img--worker'), {attr: {src: workerSprite[1]}}, 100)
+    .set(element.find('.img--worker'), {attr: {src: workerSprite[2]}}, 200)
+    .set(element.find('.img--worker'), {attr: {src: workerSprite[3]}}, 300);
+
+var scene2 = new TimelineMax();
+scene2.to(element.find('.img--worker'), 1000, {left: '70%'}, 5000)
+    .to(element.find('.img--worker'), 1000, {bottom: 50}, 6000);
+
+
+scene1Final = new TimelineMax({repeat: 0});
+scene1Final.add([boySwap, scene1Bg, scene1, scene2, workerSwap])
+
+
+
+var sceneController = new ScrollMagic.Scene({
+    duration: 8000,
+    offset: -20,
     reverse: true,
-    triggerElement: '.act_second'
+    triggerElement: '.act_first'
 })
-    .setPin('.act_second')
-    .on('start', function () {
-        scene2.play();
-    })
-    .setTween(scene2)
+    .setPin('.act_first')
+    // .on('start', function() {
+    //     scene1Final.play();
+    // })
+    .setTween(scene1Final)
+    .addIndicators({name: "boy-walk"})
     .addTo(controller);
+
